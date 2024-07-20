6922e8fbc4ebfe8dbee3f65f05ee119b475b9e4f
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-20 13:49:10 -0300

    feat: create login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputPrimaryComponent } from "../../components/input-primary/input-primary.component";
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +13,39 @@ import { InputPrimaryComponent } from "../../components/input-primary/input-prim
     DefaultLoginLayoutComponent,
     ReactiveFormsModule,
     InputPrimaryComponent
-],
+  ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor(){
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     })
+  }
+
+  submit(){
+   this.loginService.login(
+    this.loginForm.value.email,
+    this.loginForm.value.password
+   ).subscribe({
+    next: () => this.toastService.success("Login feito com sucesso!"),
+    error: (err) => this.toastService.error("Tente novamente mais tarde.", "Erro inesperado!")
+   })
+    
+  }
+
+  navigate(){
+    this.router.navigate(['/signup'])
   }
 }
