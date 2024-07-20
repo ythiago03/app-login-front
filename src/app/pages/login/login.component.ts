03ce0a842a61344616a3059dd4f3f1689e273686
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-20 13:21:42 -0300

    feat: add login input to login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputPrimaryComponent } from "../../components/input-primary/input-primary.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponent
-  ],
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    InputPrimaryComponent
+],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    })
+  }
 }
